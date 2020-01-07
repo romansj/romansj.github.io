29a3a60fbe3b48d226de98eb7f91145c98c33dc4
Jānis Romāns <30439620+romansj@users.noreply.github.com>
2020-01-07 12:05:28 +0200

    fix tag

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
+//šī ir alternatīva, ka sūtīt formu (taisīju 2 veidus, jo nezināju, ka tā web lapa nestrādā. skat. test.html)
+
 document.addEventListener("DOMContentLoaded", function() {
       document.getElementsByTagName('form')[0].addEventListener('submit', (event) => {
         event.preventDefault();
+
+        //veic pārbaudi, vai viss ir pareizi
+        //ja viss pareizi, sūti
+
         // TODO do something here to show user that form is being submitted
         fetch(event.target.action, {
           method: 'POST',
